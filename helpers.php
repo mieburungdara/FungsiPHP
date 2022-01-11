ba6b9349d003f641017c14540a4ae0826ffe83db
mieburungdara <rereitureza@gmail.com>
2022-01-12 03:14:20 +0800

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -1,1 +1,12 @@
-
+<?php
+if (! function_exists('upload_files')) {
+    /**
+     * Upload files to Telegra.ph.
+     *
+     * @param string|array $files Path to local file.
+     * @return array Array with permalinks for uploaded files.
+     */
+    function upload_files($images) {
+        return File::upload($images);
+    }
+}
