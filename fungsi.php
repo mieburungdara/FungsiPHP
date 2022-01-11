30dd4e7bf1b88e9cabc2d91d810829aefdab0cc9
mieburungdara <rereitureza@gmail.com>
2022-01-12 02:36:20 +0800

    Create fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Fungsi Class
+ *
+ * @category  PHP Function
+ * @package   Fungsi
+ * @author    Reza Fahmi <rereitureza@gmail.com>
+ * @copyright Copyright (c) 2022
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link      http://github.com/mieburungdara/FungsiPHP
+ * @version   1.0
+ */
